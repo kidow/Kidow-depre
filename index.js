50c526df80c0b08f2dca97fa119033feee22e324
kidow <wcgo2ling@gmail.com>
2019-12-18 00:12:11 +0900

    #0.1.0 initial deploy

---
+++ index.js
@@ -0,0 +1,28 @@
+const functions = require('firebase-functions')
+const { Nuxt } = require('nuxt')
+const express = require('express')
+
+const app = express()
+
+const nuxtConfig = require('./nuxt.config.js')
+
+const config = {
+  ...nuxtConfig,
+  dev: false,
+  debug: false
+}
+const nuxt = new Nuxt(config)
+
+function handleRequest(req, res) {
+  res.set('Cache-Control', 'public, max-age=300, s-maxage=600')
+  return new Promise((resolve, reject) => {
+    nuxt.ready().then(() => {
+      nuxt.render(req, res, promise => {
+        promise.then(resolve).catch(reject)
+      })
+    })
+  })
+}
+
+app.use(handleRequest)
+exports.ssrapp = functions.https.onRequest(app)
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,65 +1,72 @@
+const TITLE = 'Kidow Blog'
+const DESCRIPTION = 'Kidow Blog'
+const IMAGE = ''
+const PUBLIC_LINK = 'https://www.kidow.kr'
+const COLOR = '#e8590c'
 
-export default {
+module.exports = {
   mode: 'universal',
-  /*
-  ** Headers of the page
-  */
+  srcDir: 'src/',
   head: {
-    title: process.env.npm_package_name || '',
+    title: TITLE,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      {
+        hid: 'description',
+        name: 'description',
+        content: DESCRIPTION
+      },
+
+      // Open Graph
+      { hid: 'og-site_name', property: 'og:site_name', content: TITLE },
+      { hid: 'og-title', property: 'og:title', content: TITLE },
+      {
+        hid: 'og-description',
+        property: 'og:description',
+        content: DESCRIPTION
+      },
+      { hid: 'og-image', property: 'og:image', content: IMAGE },
+      { hid: 'og-type', property: 'og:type', content: 'article' },
+      { hid: 'og-locale', property: 'og:locale', content: 'en_US' },
+      { hid: 'og-image-width', property: 'og:image:width', content: '1200' },
+      { hid: 'og-image-height', property: 'og:image:height', content: '630' },
+      {
+        hid: 'og-image-alt',
+        property: 'og:image:alt',
+        content: '###Kidolog###'
+      },
+      { hid: 'og-url', property: 'og:url', content: PUBLIC_LINK },
+
+      // Twiiter
+      { hid: 'twitter-site', property: 'twitter:site', content: '@mostpeople' },
+      { hid: 'twitter-card', property: 'twitter:card', content: 'summary' },
+      { hid: 'twitter-title', property: 'twitter:title', content: TITLE },
+      {
+        hid: 'twitter-description',
+        property: 'twitter:description',
+        content: DESCRIPTION
+      },
+      { hid: 'twitter-image', property: 'twitter:image', content: IMAGE },
+      {
+        hid: 'twitter-domain',
+        property: 'twitter:domain',
+        content: PUBLIC_LINK
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    script: [
+      {
+        src: 'https://use.fontawesome.com/releases/v5.11.2/js/all.js'
+      }
     ]
   },
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
-  /*
-  ** Global CSS
-  */
-  css: [
-    'ant-design-vue/dist/antd.css'
-  ],
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-    '@/plugins/antd-ui'
-  ],
-  /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-  ],
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    '@nuxtjs/pwa',
-    // Doc: https://github.com/nuxt-community/dotenv-module
-    '@nuxtjs/dotenv',
-  ],
-  /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
-  axios: {
-  },
-  /*
-  ** Build configuration
-  */
+  loading: { color: COLOR },
+  css: ['ant-design-vue/dist/antd.css'],
+  plugins: ['@/plugins/antd-ui'],
+  buildModules: [],
+  modules: ['@nuxtjs/pwa', '@nuxtjs/dotenv'],
   build: {
-    /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
-    }
+    extend(config, ctx) {}
   }
 }
